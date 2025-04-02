118979a8e89d30a5e36ef4913a3a8f2c6a57ee7b
Patrick Jandron <patrick.jandron@gmail.com>
2025-04-02 10:53:44 -0400

    added new player file and related class and updated main function and constants

--- main.py
+++ main.py
@@ -3,21 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+
 
 def main():
-    pygame.init
+    pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
     game_start = 1
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
 
     while game_start > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-          
 
+        dt = clock.tick(60) / 1000
+          
 if __name__ == "__main__":
     main()
